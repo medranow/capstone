4f3307b237df07a96f4411cc765df5d62ad2cb3a
medranow <waldro4513@gmail.com>
2024-02-27 07:58:21 -0600

    working on a simple model to add a patient

--- medomai/anamnesis/models.py
+++ medomai/anamnesis/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django import forms
+
 
 # Create your models here.
 class User(AbstractUser):
     pass
 
+class Patients(models.Model):
+    name = models.CharField(max_length=25, blank=True)
+    lastname = models.CharField(max_length=25, blank=True)
+
+class Patienthistory(models.Model):
+    patient = models.ForeignKey(Patients, on_delete=models.CASCADE, blank=True, null=True, related_name="patient_history")
+    date = models.DateTimeField()
+    
