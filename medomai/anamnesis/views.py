332805d253ad40b8a450c89eafe0b92c02e29789
medranow <waldro4513@gmail.com>
2024-02-26 18:40:52 -0600

    We created our log in feature

--- medomai/anamnesis/views.py
+++ medomai/anamnesis/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from .models import User
+from django.db import IntegrityError
 
 # Create your views here.
+def index(request):
+    return render(request, "anamnesis/index.html")
+
+# Log in user
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "anamnesis/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "anamnesis/login.html")
+
+# Logout user    
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+# Register user = doctor
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        first_name = request.POST["firstname"]
+        last_name = request.POST["lastname"] 
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "anamnesis/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+        except IntegrityError:
+            return render(request, "anamnesis/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "anamnesis/register.html")
--- medomai/medomai/urls.py
+++ medomai/medomai/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('anamnesis/', include('anamnesis.urls'))
+    path("", include('anamnesis.urls'))
 ]
