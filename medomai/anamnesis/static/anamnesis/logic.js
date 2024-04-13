58c05800dc3af70e2ae610b719024d8108de2d99
medranow <waldro4513@gmail.com>
2024-04-13 12:55:53 -0600

    Some changes to CSS

--- medomai/anamnesis/static/anamnesis/logic.js
+++ medomai/anamnesis/static/anamnesis/logic.js
@@ -114,11 +114,12 @@ async function search() {
             // Create the <a> tag
             var link = document.createElement('a');
             link.href = `file/${element.id}`;
-            link.className = 'list-group-item list-group-item-action';
+            link.className = 'list-group-item list-group-item-action textCenter';
 
             // Create the <li> element
             var patient = document.createElement('li');
             patient.innerHTML = `${element.name} ${element.lastname}`;
+            patient.className = 'textCenter';
 
             // Append the <li> to the <a>
             link.appendChild(patient);
